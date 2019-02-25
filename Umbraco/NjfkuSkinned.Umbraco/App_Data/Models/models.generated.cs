9cea36ff08be12c2ea7bb76a18f5fbece251ce4d
DESKTOP-BILTQ4G\Claus <svinrider@gmail.com>
2019-02-25 08:27:01 +0100

    merge straight on master...

--- Umbraco/NjfkuSkinned.Umbraco/App_Data/Models/models.generated.cs
+++ Umbraco/NjfkuSkinned.Umbraco/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "af80ac63b7ce5600")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
